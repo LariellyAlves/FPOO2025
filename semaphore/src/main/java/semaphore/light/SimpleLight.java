4e9d3ade455eb38fd0958c55b9e93dd9e7d67059
LariellyAlves <alveslarielly@gmail.com>
2025-04-04 18:18:36 -0300

    Aula 01 - Interface and Class

---
+++ semaphore/src/main/java/semaphore/light/SimpleLight.java
@@ -0,0 +1,31 @@
+package semaphore.light;
+
+public class SimpleLight implements Light {
+
+	private OnOff state = OnOff.OFF;
+	
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isOn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isOff() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
