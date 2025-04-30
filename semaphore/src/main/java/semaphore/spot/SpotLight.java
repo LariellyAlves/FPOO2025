a432fb7e24d10ead45088e9034d7b17fc009de20
LariellyAlves <alveslarielly@gmail.com>
2025-04-29 21:42:05 -0300

    Aula08

--- semaphore/src/main/java/semaphore/spot/SpotLight.java
+++ semaphore/src/main/java/semaphore/spot/SpotLight.java
@@ -1,24 +1,28 @@
 package semaphore.spot;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Point;
 
+import semaphore.light.AbstractLight;
 import semaphore.light.Light;
 import semaphore.light.e27.AbstractLightE27;
 import semaphore.util.*;
+import semaphore.util.gui.Paintable;
 
-public class SpotLight implements TurnOnOff {
+public class SpotLight implements TurnOnOff, Paintable {
 	
-	private Point position = new Point(0,0);
-	private Dimension dimension = new Dimension(20,20);
+	private AbstractLight light;
+	private Image maskOn;
+	private Image maskOff;
+	private Point position;
+	private Dimension dimension;
 	
 	
-	private Light light;
-	private Image mask;
-	
-	public SpotLight(Image mask) {
+	public SpotLight(Image maskOn, Image maskOff) {
 		
-		this.mask = mask;
+		this.maskOn = maskOn;
+		this.maskOff = maskOff;
 	}
 	
 	public void setLight(AbstractLightE27 light) {
@@ -70,6 +74,19 @@ public class SpotLight implements TurnOnOff {
 	
 	public void setSize(int width, int heigth) {
 		this.dimension = new Dimension(width, heigth);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		int xLeft = position.x;
+		int yTop = position.y;
+		int width = dimension.width;
+		int height = dimension.height;
+		if(isOn())
+		g.drawImage(maskOn, xLeft, yTop, width, height, null);
+		else
+		g.drawImage(maskOff, xLeft, yTop, width, height, null);
 	}
 }
 	
--- semaphore/src/test/java/semaphore/spot/SpotLightTest.java
+++ semaphore/src/test/java/semaphore/spot/SpotLightTest.java
@@ -17,7 +17,7 @@ class SpotLightTest {
 			
 			//given 
 			AbstractLightE27 light = new E27LightBulb();
-			SpotLight spot = new SpotLight (null);
+			SpotLight spot = new SpotLight (null, null);
 			spot.setLight(light);
 			
 			//do action 
@@ -34,7 +34,7 @@ class SpotLightTest {
 		
 			//given 
 			AbstractLightE27 light = new E27LightBulb();
-			SpotLight spot = new SpotLight (null);
+			SpotLight spot = new SpotLight (null, null);
 			spot.setLight(light);
 			
 			//do action 
@@ -51,7 +51,7 @@ class SpotLightTest {
          void shouldReturnCloneOfPosition()
 	   	{
 	         //given
-	         SpotLight spot = new SpotLight(null);
+	         SpotLight spot = new SpotLight(null, null);
 	         spot.setLight(new E27LightBulb());
 	         final int xLeft = 20;
 	         final int yTop = 50;
@@ -74,7 +74,7 @@ class SpotLightTest {
 	   void shouldReturnCloneOfDimension() {
 		   
             //given
-            SpotLight spot = new SpotLight(null);
+            SpotLight spot = new SpotLight(null, null);
             spot.setLight(new E27LightBulb());
             final int xLeft = 20;
             final int yTop = 50;
